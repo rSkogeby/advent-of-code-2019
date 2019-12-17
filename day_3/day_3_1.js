cf1b3deb6b566e9d2ce761e27ffeb3a54e731603
Richard Skogeby <richard.skogeby@outlook.com>
2019-12-17 16:15:39 +0000

    Solved

--- day_3/day_3_1.js
+++ day_3/day_3_1.js
@@ -7,137 +7,121 @@
 // let m1 = [R75,D30,R83,U83,L12,D49,R71,U7,L72]
 // let m2 = [U62,R66,U55,R34,D71,R55,D58,R83]
 
-const fs = require('fs'); let filename = process.argv[2] || 'inp.txt'
-let f = fs.readFileSync(filename, 'utf8')
-const m = f.split('\n').filter(item => item != '')
-const m1 = m[0].split(',')
-const m2 = m[1].split(',')
-// x1-x2=m(y1-y2)
 
-let startPoint_1 = [0, 0]
-let endPoint_1 = [0, 0]
-const allPoints_1 = []
-m1.forEach(function (element) {
-  if (element[0] == 'R') {
-    const p1 = startPoint_1
-    const mxR = parseInt(element.split('R')[1])
-    endPoint_1 = [endPoint_1[0] + mxR, endPoint_1[1]]
-    const p2 = endPoint_1
-    allPoints_1.push([p1, p2])
-    startPoint_1 = endPoint_1
-  } else if (element[0] == 'L') {
-    const p1 = startPoint_1
-    const mxL = parseInt(element.split('L')[1])
-    endPoint_1 = [endPoint_1[0] - mxL, endPoint_1[1]]
-    const p2 = endPoint_1
-    allPoints_1.push([p1, p2])
-    startPoint_1 = endPoint_1
-  } else if (element[0] == 'U') {
-    const p1 = startPoint_1
-    const myU = parseInt(element.split('U')[1])
-    endPoint_1 = [endPoint_1[0], endPoint_1[1] + myU]
-    const p2 = endPoint_1
-    allPoints_1.push([p1, p2])
-    startPoint_1 = endPoint_1
-  } else if (element[0] == 'D') {
-    const p1 = startPoint_1
-    const myD = parseInt(element.split('D')[1])
-    endPoint_1 = [endPoint_1[0], endPoint_1[1] - myD]
-    const p2 = endPoint_1
-    allPoints_1.push([p1, p2])
-    startPoint_1 = endPoint_1
+function parseInputLine(line) {
+  const instructions = line.split(',')
+  let coords_current = [0,0]
+  let id = 0
+  let totalSegDistance = 0
+
+  return instructions.map(function(instruction) {
+    const direction = instruction[0]
+    const distance = parseInt(instruction.slice(1))
+    const result = {id: id++, startX: coords_current[0], startY: coords_current[1],
+      orthogonality: (direction=='R'||direction=='L')?'horizontal':'vertical', direction: direction,
+      hasIntersection: false,
+      intersections: {
+        point: [] ,
+        id: []
+      },
+      distanceUntilSegment: 0,
+      segmentLength: totalSegDistance}
+
+    switch (direction) {
+      case 'R':
+        coords_current[0] = coords_current[0] + distance
+        break
+      case 'L':
+        coords_current[0] = coords_current[0] - distance
+        break
+      case 'U':
+        coords_current[1] = coords_current[1] + distance
+        break
+      case 'D':
+        coords_current[1] = coords_current[1] - distance
+        break
+    }
+    result.distanceUntilSegment = totalSegDistance
+    totalSegDistance += distance
+    result.segmentLength = distance
+    result.endX = coords_current[0]
+    result.endY = coords_current[1]
+    return result
+  })
+}
+
+function getLineIntercept (segment1,segment2) {
+  // Return true if lines intercept, else return false
+  let intersection = {x: 0, y:0, exists: false}
+  if (segment1.orthogonality == segment2.orthogonality) {
+    intersection.exists = false
+    return intersection
   }
-})
 
-const finalsol = []
+  x1 = {i: segment1.startX, f: segment1.endX}
+  x2 = {i: segment2.startX, f: segment2.endX}
+  y1 = {i: segment1.startY, f: segment1.endY}
+  y2 = {i: segment2.startY, f: segment2.endY}
 
-let startPoint_2 = [0, 0]
-let endPoint_2 = [0, 0]
-const allPoints_2 = []
-m2.forEach(function (element) {
-  if (element[0] == 'R') {
-    const p1 = startPoint_2
-    const mxR = parseInt(element.split('R')[1])
-    endPoint_2 = [endPoint_2[0] + mxR, endPoint_2[1]]
-    const p2 = endPoint_2
-    allPoints_2.push([p1, p2])
-    startPoint_2 = endPoint_2
-  } else if (element[0] == 'L') {
-    const p1 = startPoint_2
-    const mxL = parseInt(element.split('L')[1])
-    endPoint_2 = [endPoint_2[0] - mxL, endPoint_2[1]]
-    const p2 = endPoint_2
-    allPoints_2.push([p1, p2])
-    startPoint_2 = endPoint_2
-  } else if (element[0] == 'U') {
-    const p1 = startPoint_2
-    const myU = parseInt(element.split('U')[1])
-    endPoint_2 = [endPoint_2[0], endPoint_2[1] + myU]
-    const p2 = endPoint_2
-    allPoints_2.push([p1, p2])
-    startPoint_2 = endPoint_2
-  } else if (element[0] == 'D') {
-    const p1 = startPoint_2
-    const myD = parseInt(element.split('D')[1])
-    endPoint_2 = [endPoint_2[0], endPoint_2[1] - myD]
-    const p2 = endPoint_2
-    allPoints_2.push([p1, p2])
-    startPoint_2 = endPoint_2
-  }
-})
-let intercept = []
-for (let i = 0; i < allPoints_1.length; i++) {
-  let line_seg_1 = {x_lo: Math.min(allPoints_1[i][0][0],allPoints_1[i][1][0]),
-    x_hi: Math.max(allPoints_1[i][0][0],allPoints_1[i][1][0]),
-    y_lo: Math.min(allPoints_1[i][0][1],allPoints_1[i][1][1]),
-    y_hi: Math.max(allPoints_1[i][0][1],allPoints_1[i][1][1])}
-  let d1x = (line_seg_1.x_hi - line_seg_1.x_lo)
-  let d1y = (line_seg_1.y_hi - line_seg_1.y_lo)
-
-
-  for (let j = 0; j < allPoints_2.length; j++) {
-    let line_seg_2 = {x_lo: Math.min(allPoints_2[j][0][0],allPoints_2[j][1][0]),
-                      x_hi: Math.max(allPoints_2[j][0][0],allPoints_2[j][1][0]),
-                      y_lo: Math.min(allPoints_2[j][0][1],allPoints_2[j][1][1]),
-                      y_hi: Math.max(allPoints_2[j][0][1],allPoints_2[j][1][1])}
-
-    let d2x = (line_seg_2.x_hi - line_seg_2.x_lo)
-    let d2y = (line_seg_2.y_hi - line_seg_2.y_lo)
-
-
-    // Conditions if they overlap:
-    if (d1x == 0 && d2x != 0) {     // Cond. 1: They are not parallel
-
-      // line_seg_1.x_hi == line_seg_1.x_lo
-      let line_seg_1_x = line_seg_1.x_hi // === line_seg_1.x_lo
-      if (line_seg_1_x <= line_seg_2.x_hi && line_seg_1_x >= line_seg_2.x_lo) {       // Cond. 2: They overlap in x
-        // line_seg_2.y_hi == line_seg_2.y_lo
-        let line_seg_2_y = line_seg_2.y_hi // === line_seg_1.x_lo
-        if (line_seg_2_y <= line_seg_1.y_hi && line_seg_2_y >= line_seg_1.y_lo) {         // Cond. 3: They overlap in y
-          intercept.push({x: line_seg_1_x, y: line_seg_2_y})
-        }
-      }
-    } else if (d1x != 0 && d2x == 0) {     // Cond. 1: They are not parallel
-      // line_seg_2.x_hi == line_seg_2.x_lo
-      let line_seg_2_x = line_seg_2.x_hi // === line_seg_2.x_lo
-      if (line_seg_2_x <= line_seg_1.x_hi && line_seg_2_x >= line_seg_1.x_lo) {       // Cond. 2: They overlap in x
-        // line_seg_1.y_hi == line_seg_1.y_lo
-        let line_seg_1_y = line_seg_1.y_hi // === line_seg_1.x_lo
-        if (line_seg_1_y <= line_seg_2.y_hi && line_seg_1_y >= line_seg_2.y_lo) {        // Cond. 3: They overlap in y
-          intercept.push({x: line_seg_2_x, y: line_seg_1_y})
-      }
+  if (segment1.orthogonality == 'horizontal') { // y1 =  const, x2 = const
+    if (y1.i >= Math.min(y2.i,y2.f) && y1.i <= Math.max(y2.i,y2.f) &&
+      x2.i >= Math.min(x1.i,x1.f) && x2.i <=  Math.max(x1.i,x1.f) ) { //
+      //They intersect
+      intersection.exists = true
+      intersection.y = y1.i
+      intersection.x = x2.i
+      return intersection
+    }
+  } else if (segment1.orthogonality == 'vertical') { // x1 =  const, y2 = const
+    if (x1.i >= Math.min(x2.i,x2.f) && x1.i <= Math.max(x2.i,x2.f) &&
+        y2.i >= Math.min(y1.i,y1.f) && y2.i <= Math.max(y1.i,y1.f) ) { //
+      //They intersect
+      intersection.exists = true
+      intersection.y = y2.i
+      intersection.x = x1.i
+      return intersection
     }
   }
-
-
-
-}
+  return intersection
 }
 
-  //if (intercept != null) {
-    const dist = intercept.map(point => Math.abs(point.x) + Math.abs(point.y)).filter(distance => distance != 0)
-    const min_dist = dist.reduce((mem, d) => mem=d<mem?d:mem,9999999)
-    console.log(min_dist)
+// Read file
+const fs = require('fs'); let filename = process.argv[2] || 'inp2.txt'
+let f = fs.readFileSync(filename, 'utf8')
+const m = f.split('\n').filter(item => item != '')
 
-    //if (dis > 0) finalsol.push(dis)
-  //}
+// Parse input to object
+const line_1 = parseInputLine(m[0])
+totalSegDistance = 0
+const line_2 = parseInputLine(m[1])
+
+// Add intersections to objects
+for (let segment_1 of line_1) {
+  for (let segment_2 of line_2) {
+    let lineIntercept = getLineIntercept(segment_1, segment_2)
+    if (lineIntercept.exists == false) {
+      continue
+    } else {
+      segment_1.hasIntersection = true
+      segment_1.intersections.point.push([lineIntercept.x,lineIntercept.y])
+      segment_1.intersections.id.push(segment_2.id)
+     // console.log(segment_1)
+    }
+  }
+}
+
+
+// Check where intersection occurs and filter out the origin
+segmentsWithIntersections = line_1.filter(seg => seg.hasIntersection == true && seg.id != 0)
+
+// Find manhattan distance to origin for each intersection
+let min_manhattan_distance = 9999
+for (const segment of segmentsWithIntersections) {
+  for (const point of segment.intersections.point){
+    let d = Math.abs(point[0])+Math.abs(point[1])
+    if (d < min_manhattan_distance) {
+      min_manhattan_distance = d
+    }
+  }
+}
+console.log('The minimum Manhattan distance is ' + min_manhattan_distance)
