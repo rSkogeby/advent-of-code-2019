ec0d65b9f94674298f4fae164dfecba1e7afa82a
Richard Skogeby <richard.skogeby@outlook.com>
2019-12-21 14:23:58 +0000

    Fix calculation of angle

--- day_10/day_10_1.js
+++ day_10/day_10_1.js
@@ -6,13 +6,14 @@ const m = f.split('\n').filter(item => item != '')
 
 // check coordinate of each asteroid
 // for each asteroid calculate number of unique angles to other asteroids
-// asteroid with most unique angles wins
+// asteroid with the highest number of unique angles wins
 
 const grid = {
   x: m[0].length,
   y: m.length
 }
 
+// check coordinate of each asteroid
 let asteroids = []
 for (let x = 0; x < grid.x; x++) {
   for (let y = 0; y < grid.y; y++) {
@@ -22,35 +23,37 @@ for (let x = 0; x < grid.x; x++) {
   }
 }
 
+// for each asteroid calculate number of unique angles to other asteroids
 let asteroidMetaData = []
 for (let monitoringStation of asteroids) {
-  //console.log(asteroid)
   let uniqueAngles = new Set()
   for (let asteroid of asteroids) {
     if (asteroid != monitoringStation) {
       let prospect = getAngle(monitoringStation, asteroid)
       let angle = prospect.angle
-      let quadrant = prospect.quadrant
-      if (quadrant == 0) {
-        angle = angle + 180
-      } else if (quadrant == 1) {
-        angle = angle
-      }
-      if (!uniqueAngles.has([angle,quadrant])) {
+      if (!uniqueAngles.has(angle)) {
         uniqueAngles.add(angle)
       }
     }
   }
-  asteroidMetaData.push({coordinates: monitoringStation, asteroidsDetected: uniqueAngles.size })
+  asteroidMetaData.push({coordinates: monitoringStation, asteroidsDetected: uniqueAngles.size , asts: uniqueAngles})
 }
 
-maxNumAsteroidsDetected = asteroidMetaData.reduce((mem,asteroid) => mem = (mem<asteroid.asteroidsDetected) ? asteroid.asteroidsDetected : mem, 0)
-console.log('The maximum number of asteroids you can detect from any other asteroid is ' + maxNumAsteroidsDetected)
+// asteroid with higest number of unique angles wins
+monitoringStation = asteroidMetaData.reduce((mem,asteroid) => mem = (mem.asteroidsDetected<asteroid.asteroidsDetected) ? asteroid : mem, {asteroidsDetected: 0})
+console.log('The maximum number of asteroids you can detect from any other asteroid is ' + monitoringStation.asteroidsDetected)
 
 function getAngle(monitoringStation, asteroid) {
   let deltaX = asteroid.x - monitoringStation.x
   let deltaY = asteroid.y - monitoringStation.y
-  let quadrant = (deltaX >= 0) ? 1:0
-  let angle = Math.atan(deltaY/deltaX)*180/Math.PI + 90
-  return {asteroid: asteroid, angle: angle, quadrant: quadrant}
+  let semisphere = (deltaX > 0) ? 1:0
+  let angle = Math.atan(deltaY/deltaX) * 180/Math.PI
+  if (semisphere == 0) {
+    angle = angle + 180
+  } else if (semisphere == 1) {
+    if (angle < 0) {
+      angle = angle + 360
+    }
+  }
+  return {asteroid: asteroid, angle: angle}
 }
